8d1bbbb615c5e62b5b418ec8810ddf5117a44737
Naveen Guleria <guleriagishere@gmail.com>
2020-12-30 17:24:11 +0530

    mono repo for react
    mono repo for react

--- config/mono.sh
+++ config/mono.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-if [ $(git show -s --format=%s | grep "node")  -gt 0 ]; then
-    echo "trigger node app."
-    aws codepipeline start-pipeline-execution --name monorepo-nodeapp
-elif [ $(git show -s --format=%s | grep "react")  -gt 0 ]; then
-    echo "trigger react app."
-    aws codepipeline start-pipeline-execution --name monorepo-react
-else 
-    echo "trigger terraform app"
-    aws codepipeline start-pipeline-execution --name monorepo-terrci
-fi
-
-
-#!/bin/bash
-
+set +x
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 
   case "$MESSAGE" in
