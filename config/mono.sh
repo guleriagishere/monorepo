9c4d56d426a8f9a0ac5cf1e231d3e0dd67bdc3af
Naveen Guleria <guleriagishere@gmail.com>
2020-12-30 17:19:43 +0530

    mono repo for react.

--- config/mono.sh
+++ config/mono.sh
@@ -12,3 +12,24 @@ else
 fi
 
 
+#!/bin/bash
+
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+
+  case "$MESSAGE" in
+    *node*)
+       echo "node pipeline."
+       aws codepipeline start-pipeline-execution --name monorepo-nodeapp
+        ;;
+    *react*) 
+        echo "React pipeline"
+        aws codepipeline start-pipeline-execution --name monorepo-react
+        ;; 
+    *terraform*)  
+        echo "Terraform pipeline."
+        aws codepipeline start-pipeline-execution --name monorepo-terrci
+        ;;
+    default)
+        echo "no matches"
+        ;;
+  esac
