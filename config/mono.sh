4ee15769cf1f1117f62ef01a75a92c691fc02716
Naveen Guleria <guleriagishere@gmail.com>
2021-01-01 16:16:54 +0530

    s3 upload

--- config/mono.sh
+++ config/mono.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-set +x
-MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+if [ $(git show -s --format=%s | grep "node")  -gt 0 ]; then
+    echo "trigger node app."
+    echo "Uploading node data to s3 bucket."
+    zip -r node.zip ../node-api
+    aws s3 cp node.zip monorepo-bucket/
+    # aws codepipeline start-pipeline-execution --name monorepo-nodeapp
+elif [ $(git show -s --format=%s | grep "react")  -gt 0 ]; then
+    echo "trigger react app."
+    echo "Uploading react data to s3 bucket."
+    zip -r react.zip ../react
+    aws s3 cp react.zip monorepo-bucket/
+    # aws codepipeline start-pipeline-execution --name monorepo-react
+else 
+    echo "trigger terraform app"
+    echo "Uploading terraform data to s3 bucket."
+    aws codepipeline start-pipeline-execution --name monorepo-terrci
+fi
 
-  case "$MESSAGE" in
-    *node*)
-       echo "node pipeline."
-       aws codepipeline start-pipeline-execution --name monorepo-nodeapp
-        ;;
-    *react*) 
-        echo "React pipeline"
-        aws codepipeline start-pipeline-execution --name monorepo-react
-        ;; 
-    *terraform*)  
-        echo "Terraform pipeline."
-        aws codepipeline start-pipeline-execution --name monorepo-terrci
-        ;;
-    default)
-        echo "no matches"
-        ;;
-  esac
+
