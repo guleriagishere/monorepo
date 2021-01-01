09f162e6ff807f55bca357b4b7a7c88d2f3218a1
Naveen Guleria <guleriagishere@gmail.com>
2021-01-01 16:22:53 +0530

    s3 upload case

--- config/mono.sh
+++ config/mono.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-if [ $(git show -s --format=%s | grep "node")  -gt 0 ]; then
-    echo "trigger node app."
-    echo "Uploading node data to s3 bucket."
-    zip -r node.zip ../node-api
-    aws s3 cp node.zip monorepo-bucket/
-    # aws codepipeline start-pipeline-execution --name monorepo-nodeapp
-elif [ $(git show -s --format=%s | grep "react")  -gt 0 ]; then
-    echo "trigger react app."
-    echo "Uploading react data to s3 bucket."
-    zip -r react.zip ../react
-    aws s3 cp react.zip monorepo-bucket/
-    # aws codepipeline start-pipeline-execution --name monorepo-react
-else 
-    echo "trigger terraform app"
-    echo "Uploading terraform data to s3 bucket."
-    aws codepipeline start-pipeline-execution --name monorepo-terrci
-fi
-
-
+set +x
+  case "$MESSAGE" in
+    *node*)
+        echo "node pipeline."
+        echo "Uploading node data to s3 bucket."
+        zip -r node.zip ../node-api
+        aws s3 cp node.zip monorepo-bucket/   
+        # aws codepipeline start-pipeline-execution --name monorepo-nodeapp
+        ;;
+    *react*) 
+        echo "React pipeline"
+        echo "Uploading react data to s3 bucket."
+        zip -r react.zip ../react
+        aws s3 cp react.zip monorepo-bucket/        
+        # aws codepipeline start-pipeline-execution --name monorepo-react
+        ;; 
+    *terraform*)  
+        echo "Terraform pipeline."
+        echo "Uploading terraform data to s3 bucket."
+        zip -r terraform.zip ../react
+        aws s3 cp terraform.zip monorepo-bucket/          
+        # aws codepipeline start-pipeline-execution --name monorepo-terrci
+        ;;
+    default)
+        echo "no matches"
+        ;;
+  esac
