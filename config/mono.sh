6d3e7e2b62fee53f363784e39ad1ae3244a4491d
Naveen Guleria <guleriagishere@gmail.com>
2021-01-01 17:18:06 +0530

    node s3 typo.
    node s3 typo.

--- config/mono.sh
+++ config/mono.sh
@@ -9,25 +9,24 @@ MESSAGE=$(git log -1 HEAD --pretty=format:%s)
         echo "node pipeline."
         echo "Uploading node data to s3 bucket."
         zip -r node.zip ../node-api
-        aws s3 cp node.zip monorepo-bucket/   
+        aws s3 cp node.zip s3://monorepo-bucket/   
         # aws codepipeline start-pipeline-execution --name monorepo-nodeapp
         ;;
     *react*) 
         echo "React pipeline"
         echo "Uploading react data to s3 bucket."
         zip -r react.zip ../react
-        aws s3 cp react.zip monorepo-bucket/        
+        aws s3 cp react.zip s3://monorepo-bucket/        
         # aws codepipeline start-pipeline-execution --name monorepo-react
         ;; 
     *terraform*)  
         echo "Terraform pipeline."
         echo "Uploading terraform data to s3 bucket."
         zip -r terraform.zip ../react
-        aws s3 cp terraform.zip monorepo-bucket/          
+        aws s3 cp terraform.zip s3://monorepo-bucket/          
         # aws codepipeline start-pipeline-execution --name monorepo-terrci
         ;;
     default)
         echo "no matches"
         ;;
   esac
-
